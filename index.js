a8ab0005b4af73063986135a61936a32f75a9643
Mattie Gonzalez <gonzalem@lssc.edu>
2022-11-28 18:01:27 -0500

    Changing Layout

--- index.js
+++ index.js
@@ -1,8 +1,66 @@
 // TODO: Include packages needed for this application
 
 const inquirer = require('inquirer');
-const fs = require('fs');
-const { default: test } = require('node:test');
+
+const { writeFile } = require('fs').promises;
+
+
+// TODO: Create an array of questions for user input
+const promptUser = () => {
+    return inquirer.prompt ([
+    //Title of Project
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      //Description of Project
+      {
+          type: 'input',
+          name: 'description',
+          message: 'Description of your project?',
+      },
+  
+      //Installations
+      {
+          type: 'input',
+          name: 'Installations',
+          message: 'Installations?',
+      },
+      //Contributions
+      {
+          type: 'input',
+          name: 'description',
+          message: 'Description of your project?',
+      },
+      //Tests
+      {
+          type: 'input',
+          name: 'description',
+          message: 'Description of your project?',
+      },
+      //Questions
+      //Email
+      {
+          type: 'input',
+          name: 'email',
+          message: 'Please Enter your Email Address?',
+      },
+      //Github
+      {
+          type: 'input',
+          name: 'github',
+          message: 'Please Enter your Github Username?',
+      },
+       //License
+       {
+          type: 'list',
+          name: 'License',
+          message: 'Which License is affiliated with you project?',
+          choices:[],
+       },
+    ]);
+};
 
 //README file formatting
 const generateReadme = ({ title, description, installation, usage, contributions, tests, email, github, license }) =>
@@ -26,73 +84,14 @@ const generateReadme = ({ title, description, installation, usage, contributions
   Github: www.github.com/${github}
   ## License © ${license} All Rights Reserved.`;
 
-// TODO: Create an array of questions for user input
-inquirer
-  .prompt([
-    //Title of Project
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of your project?',
-    },
-    //Description of Project
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Description of your project?',
-    },
+//Create a function to initialize app
+const init = () => {
+    promptUser()
+        //Write to README.md file
+        .then((answers) => writeFile('README.md', generateReadme(answers)))
+        .then(() => console.log('Successfully wrote to README.md'))
+        .catch((err) => console.error(err));        
+};
 
-    //Installations
-    {
-        type: 'input',
-        name: 'Installations',
-        message: 'Installations?',
-    },
-    //Contributions
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Description of your project?',
-    },
-    //Tests
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Description of your project?',
-    },
-    //Questions
-    //Email
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Please Enter your Email Address?',
-    },
-    //Github
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Please Enter your Github Username?',
-    },
-     //License
-     {
-        type: 'list',
-        name: 'License',
-        message: 'Which License is affiliated with you project?',
-        choices:[],
-     },
-  ])
-
-.then((answers) => {
-    const readmePageContent = generateReadme(answers);
-
-    fs.writeFile('README.md', readmePageContent, (err) =>
-    err ? console.log(err) : console.log('Successfully created README.md'))
-})
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
+//Function call to initialize app
+init();
