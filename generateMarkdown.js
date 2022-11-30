8ce7dba493573207e516c30994bf084b1bde5931
Mattie Gonzalez <gonzalem@lssc.edu>
2022-11-29 21:04:56 -0500

    Starting Licenses

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,14 +1,25 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// MIT License
+function renderLicenseBadge(mitLicense) {
+  [![apacheLicense]('https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0')]
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// GPLv2 License
+function renderLicenseLink(gplv2License) {
+  [![gplv2License]('https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html')]
+}
+
+// Apache License
+function renderLicenseBadge(apacheLicense) {
+  [![apacheLicense]('https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0')]
+}
+
+// GPLv3 License
+function renderLicenseSection(gplv3license) {
+  [![gplv3license]('https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0')]
+}
+
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
